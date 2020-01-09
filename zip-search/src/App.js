3f36f95dc35f55d4bc59aa12bb648b35be165f0b
Brittany Cho <bac411@nyu.edu>
2020-01-08 23:23:51 -0500

    added city-search api

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
 import './App.css';
 
+class CitySearcher extends Component {
+    constructor(props) {
+	    super(props);
+
+      this.state = {
+          data: this.props.data
+      }
+	
+	    this.search = this.search.bind(this);
+    }
+
+    search() {
+      axios.get("http://ctp-zip-api.herokuapp.com/city/" + (document.getElementById("inputCity").value).toUpperCase())
+          .then((response) => {
+            console.log(response);
+        })
+          .then((error) => {
+            console.log(error);
+        })
+    }
+
+    render() {
+	return (
+		<div>
+		City Searcher <br/>
+		<input type="text" id="inputCity"/> <br/>
+		<button onMouseDown={this.search}> Search </button>
+		<div> {this.state.data} </div>
+		</div>
+		);
+		
+    }
+}
+
+CitySearcher.propTypes = {
+    data: PropTypes.array
+};
+
+CitySearcher.defaultProps = {
+    data: []
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+	    <div class="App-header">
+	     <CitySearcher/>
+	     </div>
+	     );
 }
 
 export default App;
+
