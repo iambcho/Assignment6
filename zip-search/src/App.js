4f22dfa66a65bd0d89a39e75b39142680eb8b69e
Brittany Cho <bac411@nyu.edu>
2020-01-09 12:02:17 -0500

    added parsing

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -19,19 +19,57 @@ class CitySearcher extends Component {
       axios.get("http://ctp-zip-api.herokuapp.com/city/" + (document.getElementById("inputCity").value).toUpperCase())
           .then((response) => {
             console.log(response);
+            console.log(typeof response);
+            console.log(response.data);
+            console.log(response.data[0]);
+
+            let table = [];
+
+            for(let i = 0; i < response.data.length; i++) {
+              console.log(response.data[i]);
+              table.push(<h3>{response.data[i]}</h3>)
+            }
+            
+
+            
+            this.setState({ data: table });
+            
+
         })
           .then((error) => {
             console.log(error);
         })
     }
 
+    // createTable = () => {
+
+    //   if(this.state.data.length > 0) {
+
+    //     for (let i = 0; i < 3; i++) {
+    //       let children = []
+    //       //Inner loop to create children
+    //       for (let j = 0; j < 5; j++) {
+    //         children.push(<td>{`Column ${j + 1}`}</td>)
+    //       }
+    //       //Create the parent and add the children
+    //       table.push(<tr>{children}</tr>)
+    //     }
+    //     return table
+
+    //   }
+
+    // }
+
     render() {
+
 	return (
+    
 		<div>
 		City Searcher <br/>
 		<input type="text" id="inputCity"/> <br/>
 		<button onMouseDown={this.search}> Search </button>
 		<div> {this.state.data} </div>
+    {/* <div>this.createTable()</div> */}
 		</div>
 		);
 		
